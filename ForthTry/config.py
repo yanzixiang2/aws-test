6814ad8f0ee376d060515badf07fa7a41044bcb2
yanzixiang2 <143473755+yanzixiang2@users.noreply.github.com>
2023-09-23 01:19:44 +0800

    Update config.py

--- ForthTry/config.py
+++ ForthTry/config.py
@@ -2,7 +2,7 @@ customhost = "employee.cxu6wetzoblv.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "looichenyong-employee1"
+custombucket = "looichenyong-employee"
 customregion = "us-east-1"
 
 
